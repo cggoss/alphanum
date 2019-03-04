83147a92ee70c78cc2ac873899712b5b7571ebbc
cggoptimix <47851979+cggoptimix@users.noreply.github.com>
2019-03-04 17:35:37 +0100

    Reverted to original version from http://www.davekoelle.com/files/alphanum.js. Export methods `alphanum()` and `alphanumCase()` instead of exposing `Array.prototype.alphanumSort()`. `Array.prototype.alphanumSort()` has been removed.

--- README.md
+++ README.md
@@ -6,28 +6,3 @@ alphanum sorter
 Alphanum sorter is based on Dave Koelle algorithm.
 
 This npm module is based on original work published at http://www.davekoelle.com/files/alphanum.js
-
-Installing
-----------
-```
-    npm i alphanum
-```
-
-How to use?
------------
-
-```javascript
-    require("alphanum");
-
-
-    my_list=['1.10', '1.1', '1.2'];
-    my.list.sort(key=alphanum);
-```
-
-Development
------------
-
-Running tests:
-```
-    npm test
-```
--- alphanum.js
+++ alphanum.js
@@ -32,57 +32,24 @@
  *
  * Version 1.1
  *  - Fix for sorting "1.10" after "1.2" (ssbarnea)
+ * Version 1.2
+ *  - Reverted to original version from http://www.davekoelle.com/files/alphanum.js and removed Array.prototype.alphanumSort()
+ *  - export methods alphanum and alphanumCase
  */
-Array.prototype.alphanumSort = function(caseInsensitive) {
-  var z;
-  for (z = 0, t; t == this[z]; z++) {
-    this[z] = [];
-    var x = 0, y = -1, n = 0, i, j;
-
-    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
-      var m = (i >=48 && i <= 57);
-      if (m !== n) {
-        this[z][++y] = "";
-        n = m;
-      }
-      this[z][y] += j;
-    }
-  }
-
-  this.sort(function(a, b) {
-    for (var x = 0, aa, bb; (aa = a[x]) && (bb = b[x]); x++) {
-      if (caseInsensitive) {
-        aa = aa.toLowerCase();
-        bb = bb.toLowerCase();
-      }
-      if (aa !== bb) {
-        var c = Number(aa), d = Number(bb);
-        if (c == aa && d == bb) {
-          return c - d;
-        } else return (aa > bb) ? 1 : -1;
-      }
-    }
-    return a.length - b.length;
-  });
-
-  for (z = 0; z < this.length; z++)
-    this[z] = this[z].join("");
-};
-
 
 /* ********************************************************************
- * Alphanum sort() function version - case sensitive by default
+ * Alphanum sort() function version - case sensitive
  *  - Slower, but easier to modify for arrays of objects which contain
  *    string properties
  *
  */
 function alphanum(a, b) {
   function chunkify(t) {
-    var tz = [];
+    var tz = new Array();
     var x = 0, y = -1, n = 0, i, j;
 
     while (i = (j = t.charAt(x++)).charCodeAt(0)) {
-      var m = (i >=48 && i <= 57);
+      var m = (i == 46 || (i >=48 && i <= 57));
       if (m !== n) {
         tz[++y] = "";
         n = m;
@@ -115,11 +82,11 @@ function alphanum(a, b) {
  */
 function alphanumCase(a, b) {
   function chunkify(t) {
-    var tz = [];
+    var tz = new Array();
     var x = 0, y = -1, n = 0, i, j;
 
-    while (i == (j = t.charAt(x++)).charCodeAt(0)) {
-      var m = (i >=48 && i <= 57);
+    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+      var m = (i == 46 || (i >=48 && i <= 57));
       if (m !== n) {
         tz[++y] = "";
         n = m;
@@ -142,3 +109,8 @@ function alphanumCase(a, b) {
   }
   return aa.length - bb.length;
 }
+
+module.exports = {
+  alphanum: alphanum,
+  alphanumCase: alphanumCase
+};
